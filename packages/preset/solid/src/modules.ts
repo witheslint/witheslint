0b40e312fd1ed35bc4e54f4cd8671b67b8cd6288
Joie <leaps@live.com>
2023-12-24 13:17:01 +0800

    feat: add preset for solid

---
+++ packages/preset/solid/src/modules.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/newline-after-import */
+
+import { interopDefault } from '@witheslint/core'
+
+// @ts-expect-error missing types
+import * as solidPlugin from 'eslint-plugin-solid'
+export const pluginSolid = interopDefault(solidPlugin)
