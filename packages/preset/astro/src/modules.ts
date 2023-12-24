0ce5f6150774ba2e8a93904dfd0192574eb0c319
Joie <leaps@live.com>
2023-12-24 13:14:31 +0800

    feat: add preset for astro

---
+++ packages/preset/astro/src/modules.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable import/first */
+import { interopDefault } from '@witheslint/core'
+
+import * as astroPlugin from 'eslint-plugin-astro'
+export const pluginAstro = interopDefault(astroPlugin)
+
+import * as astroParser from 'astro-eslint-parser'
+export const parserAstro = interopDefault(astroParser)
