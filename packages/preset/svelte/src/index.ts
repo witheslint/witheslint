9cdd096a24d75d35ca2ac113589d7de747c58c97
Joie <leaps@live.com>
2024-01-09 14:13:23 +0800

    feat: customize `svelte` rules

--- packages/preset/svelte/src/index.ts
+++ packages/preset/svelte/src/index.ts
@@ -7,8 +7,9 @@ export const GLOB_SVELTE = '**/*.svelte'
 export const presetSvelte = () => definePreset({
   name: 'preset:svelte',
   extensions: ['svelte'],
-  setup: ({ features }) => {
-    const { typescript } = features
+  setup: ({ features, styles }) => {
+    const { typescript, stylistic } = features
+    const { indent, quotes } = styles
     return [
       { plugins: { svelte: pluginSvelte } },
       {
@@ -23,7 +24,41 @@ export const presetSvelte = () => definePreset({
         },
         processor: pluginSvelte.processors!['.svelte'],
         rules: {
-          ...pluginSvelte.configs['recommended'].rules,
+          'svelte/comment-directive': 'error',
+          'svelte/no-at-debug-tags': 'warn',
+          'svelte/no-at-html-tags': 'error',
+          'svelte/no-dupe-else-if-blocks': 'error',
+          'svelte/no-dupe-style-properties': 'error',
+          'svelte/no-dupe-use-directives': 'error',
+          'svelte/no-dynamic-slot-name': 'error',
+          'svelte/no-export-load-in-svelte-module-in-kit-pages': 'error',
+          'svelte/no-inner-declarations': 'error',
+          'svelte/no-not-function-handler': 'error',
+          'svelte/no-object-in-text-mustaches': 'error',
+          'svelte/no-reactive-functions': 'error',
+          'svelte/no-reactive-literals': 'error',
+          'svelte/no-shorthand-style-property-overrides': 'error',
+          'svelte/no-unknown-style-directive-property': 'error',
+          'svelte/no-unused-svelte-ignore': 'error',
+          'svelte/no-useless-mustaches': 'error',
+          'svelte/require-store-callbacks-use-set-param': 'error',
+          'svelte/system': 'error',
+          'svelte/valid-compile': 'error',
+          'svelte/valid-each-key': 'error',
+
+          ...stylistic
+            ? {
+                'style/no-trailing-spaces': 'off', // superseded by svelte/no-trailing-spaces
+                'svelte/derived-has-same-inputs-outputs': 'error',
+                'svelte/html-closing-bracket-spacing': 'error',
+                'svelte/html-quotes': ['error', { prefer: quotes }],
+                'svelte/indent': ['error', { alignAttributesVertically: true, indent }],
+                'svelte/mustache-spacing': 'error',
+                'svelte/no-spaces-around-equal-signs-in-attribute': 'error',
+                'svelte/no-trailing-spaces': 'error',
+                'svelte/spaced-html-comment': 'error',
+              }
+            : {},
         },
       },
     ]
