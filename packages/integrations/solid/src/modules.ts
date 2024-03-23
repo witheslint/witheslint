66448365d03a3ec6b640e6901f6e0de933e6e197
Joie <leaps@live.com>
2024-03-23 17:37:48 +0800

    fix: add type annotation for `pluginSolid`

--- packages/integrations/solid/src/modules.ts
+++ packages/integrations/solid/src/modules.ts
@@ -1,8 +1,7 @@
 import { interopDefault } from '@witheslint/core'
 
-// @ts-expect-error missing types
 import * as solidPlugin from 'eslint-plugin-solid'
 /**
  * @see [documentation](https://github.com/solidjs-community/eslint-plugin-solid)
  */
-export const pluginSolid = interopDefault(solidPlugin)
+export const pluginSolid: typeof solidPlugin = interopDefault(solidPlugin)
