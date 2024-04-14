881cfc4fc432f74797058c8fdf5d0ecb4dabaffc
Joie <leaps@live.com>
2024-04-14 20:05:24 +0800

    chore: cleanup deprecated pkg

--- deprecated/generate-dts/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Rule } from 'eslint'
-
-export type PluginRules = Record<string, Rule.RuleModule>
-
-export interface Plugin {
-  name: string
-  module: string
-  rules?: PluginRules
-}
