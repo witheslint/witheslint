166dfef6feeaef54d649124c68cd4391f5e93b30
Joie <leaps@live.com>
2024-07-13 16:02:44 +0700

    feat: update astro plugin to v1.0

---
+++ .changeset/breezy-monkeys-stare.md
@@ -0,0 +1,11 @@
+---
+"@witheslint/preset-astro": minor
+"@witheslint/core": minor
+"@witheslint/preset-react": minor
+"@witheslint/preset-solid": minor
+"@witheslint/preset-svelte": minor
+"@witheslint/preset-vue": minor
+"witheslint": minor
+---
+
+feat: update astro plugin to v1.0
--- packages/integrations/astro/src/preset.ts
+++ packages/integrations/astro/src/preset.ts
@@ -11,7 +11,7 @@ export const presetAstro = (): Preset => definePreset({
   extensions: ['astro'],
   ignores: [...GLOB_ASTRO_EXCLUDES],
   setup: ({ features }) => {
-    const { typescript } = features
+    const { typescript, stylistic } = features
     return [
       {
         name: 'witheslint:astro:plugins',
@@ -30,17 +30,30 @@ export const presetAstro = (): Preset => definePreset({
         },
         processor: pluginAstro.processors!['.astro'],
         rules: {
+          'astro/missing-client-only-directive-value': 'error',
           'astro/no-conflict-set-directives': 'error',
           'astro/no-deprecated-astro-canonicalurl': 'error',
           'astro/no-deprecated-astro-fetchcontent': 'error',
           'astro/no-deprecated-astro-resolve': 'error',
           'astro/no-deprecated-getentrybyslug': 'error',
+          'astro/no-set-html-directive': 'off',
           'astro/no-set-text-directive': 'error',
           'astro/no-unused-define-vars-in-style': 'error',
           'astro/prefer-class-list-directive': 'error',
           'astro/prefer-object-class-list': 'error',
           'astro/prefer-split-class-list': 'error',
+          'astro/semi': 'off',
           'astro/valid-compile': 'error',
+
+          ...stylistic
+            ? {
+                'style/indent': 'off',
+                'style/jsx-closing-tag-location': 'off',
+                'style/jsx-indent': 'off',
+                'style/jsx-one-expression-per-line': 'off',
+                'style/no-multiple-empty-lines': 'off',
+              }
+            : {},
         },
       },
     ]
